b8f93edf178c7f86f9c9e20eacd259d31092f7ca
maximugisha <maximugisha@gmail.com>
2024-09-27 11:27:50 +0300

    add simple signip

---
+++ app/ui/signup-form.tsx
@@ -0,0 +1,174 @@
+'use client';
+import Cookies from 'js-cookie';
+
+import { useState } from 'react';
+import { lusitana } from '@/app/ui/fonts';
+import { AtSymbolIcon, KeyIcon } from '@heroicons/react/24/outline';
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { Button } from './button';
+import { useRouter } from 'next/navigation';
+
+export default function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [first_name, setFirstName] = useState('');
+    const [last_name, setLastName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+        const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL as string;
+        const endpoint = '/api/account/signup/';
+        const url = `${baseUrl}${endpoint}`;
+        console.log(url);
+        e.preventDefault();
+        setError(''); // Reset the error message
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    email: email,
+                    first_name: first_name,
+                    last_name: last_name,
+                    password,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Registration Failed');
+            }
+
+            const data = await response.json();
+            const { access } = data;
+
+            // Redirect to another page after successful registration
+            router.push('/login'); // Replace with your redirect path
+        } catch (error) {
+            setError('Registration failed. Please check your details.');
+        }
+    };
+
+    return (
+        <form className="space-y-3" onSubmit={handleSignUp}>
+            <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+                <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+                    Welcome to Kids Diy Platfrom.
+                </h1>
+                {error && <p className="text-red-600">{error}</p>}
+                <div className="w-full">
+                    <div>
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                            htmlFor="username"
+                        >
+                            Username
+                        </label>
+                        <div className="relative">
+                            <input
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="username"
+                                type="text"
+                                name="username"
+                                placeholder="Enter your Username"
+                                required
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                        </div>
+                    </div>
+                    <div>
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                            htmlFor="email"
+                        >
+                            Email
+                        </label>
+                        <div className="relative">
+                            <input
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="Email Address"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                            htmlFor="name"
+                        >
+                            First Name
+                        </label>
+                        <div className="relative">
+                            <input
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="first_name"
+                                type="text"
+                                name="first_name"
+                                placeholder="First Name"
+                                required
+                                value={first_name}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                            htmlFor="name"
+                        >
+                            Last Name
+                        </label>
+                        <div className="relative">
+                            <input
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="last_name"
+                                type="text"
+                                name="last_name"
+                                placeholder="Last Name"
+                                required
+                                value={last_name}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className="mt-4">
+                        <label
+                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                            htmlFor="password"
+                        >
+                            Password
+                        </label>
+                        <div className="relative">
+                            <input
+                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                id="password"
+                                type="password"
+                                name="password"
+                                placeholder="Enter password"
+                                required
+                                minLength={6}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                        </div>
+                    </div>
+                </div>
+                <Button className="mt-4 w-full">
+                    Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+                </Button>
+            </div>
+        </form >
+    );
+}
