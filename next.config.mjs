8ce6fc9d2baafdcfeb3822eb53aac10e32036ca2
maximugisha <maximugisha@gmail.com>
2024-10-01 15:20:08 +0300

    rectify

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: '127.0.0.1',
-          pathname: '/media/**', // Path to the images
-        },
-        {
-            protocol: 'https',
-            hostname: 'kids-49ee77c1-f112-45e6-9897.renu-01.cranecloud.io',
-            pathname: '/media/**',
-          },
-      ],
-    },
-  };
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '147.182.223.147',
+        pathname: '/media/**', // Path to the images
+      },
+      {
+        protocol: 'https',
+        hostname: '147.182.223.147',
+        pathname: '/media/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
