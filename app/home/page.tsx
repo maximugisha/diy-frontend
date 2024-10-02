989fe139ca6951fb1f3cfc736fdb93f573d826c9
maximugisha <maximugisha@gmail.com>
2024-10-02 22:34:39 +0300

    htps

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [content, setContent] = useState('');
   const [imageFiles, setImageFiles] = useState<File[]>([]);
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL as string || 'http://147.182.223.147:8000';
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL as string || 'https://maxicodes.com:8000';
 
   useEffect(() => {
     const fetchPosts = async () => {
